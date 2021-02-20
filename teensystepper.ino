1794d4d87d56fd47e05f23a2427e36dc65ce8298
Dan Quinlivan <darknrgy@gmail.com>
2021-02-20 00:27:01 -0800

    Create Dimension class to organize code

--- teensystepper.ino
+++ teensystepper.ino
@@ -1,5 +1,6 @@
-#include "travel.h"
 #include "stepper.h"
+#include "travel.h"
+
 
 #define MOTOR_COUNT 2
 #define PIN_X_STEP 13
@@ -12,56 +13,51 @@ IntervalTimer timer;
 
 unsigned long elapsed_u_sec = 0;
 
-struct Motors {
-	TravelItem travel_item;
-	Travel travel;
-	Stepper stepper;
-} motors[MOTOR_COUNT];
-
+Dimension dims[2];
 
 void set_gimbal(int x, int y) {
 	static float t = (float) elapsed_u_sec / 1000000;
 
 	noInterrupts();
-	travel_create(&motors[0].travel, &motors[0].travel_item, MAX_V, MAX_A, t, x);
-	travel_create(&motors[1].travel, &motors[1].travel_item, MAX_V, MAX_A, t, y);
+	dims[0].solve_for_min_time(MAX_V, MAX_A, t, x);
+	dims[1].solve_for_min_time(MAX_V, MAX_A, t, y);
 	interrupts();
 }
 
 void setup() {
 	Serial.begin(9600);
 
-	motors[0].stepper.step_pin = PIN_X_STEP;
-	motors[0].stepper.dir_pin = PIN_X_DIR;
-	motors[1].stepper.step_pin = PIN_Y_STEP;
-	motors[1].stepper.dir_pin = PIN_Y_DIR;
-
 	pinMode(PIN_X_STEP, OUTPUT);
 	pinMode(PIN_X_DIR, OUTPUT);
 	pinMode(PIN_Y_STEP, OUTPUT);
 	pinMode(PIN_Y_DIR, OUTPUT);
 
+	dims[0].set_stepper_pins(PIN_X_STEP, PIN_X_DIR);
+	dims[1].set_stepper_pins(PIN_Y_STEP, PIN_Y_DIR);
+
 	timer.begin(tick, TICK_INTERVAL);  // 50 = 100us, or 10kHz
 }
 
 void tick() {
-	static volatile float elapsed_sec;
+	
 	static volatile int i;
 	static volatile bool can_reset_time;
 
 	noInterrupts();
-	can_reset_time = true;
+
 	elapsed_u_sec += TICK_INTERVAL;
-	elapsed_sec = (float) elapsed_u_sec / 1000000;
+	
+	can_reset_time = true;
 	for (i = 0; i < MOTOR_COUNT; i++) {
-		travel_tick(&motors[i].travel, &motors[i].travel_item, elapsed_sec);
-  		motors[i].stepper.p = motors[i].travel_item.p;
-		stepper_step(&motors[i].stepper, elapsed_u_sec);	
-		if (motors[i].travel.enable) {
+		dims[i].tick(elapsed_u_sec);
+		if (dims[i].is_enabled()) {
 			can_reset_time = false;
 		}
-	}  	
+
+	}
+
 	if (can_reset_time) elapsed_u_sec = 0;
+
 	interrupts();
 }
 
--- travel.h
+++ travel.h
@@ -25,121 +25,148 @@ struct Travel {
 	int dir =1;
 };
 
-struct TravelItem {
+struct Travel_Item {
 	float p = 0; // position
 	float v = 0; // velocity
 };
 
-void travel_create(Travel* travel, TravelItem *travel_item, float maxv, float maxa, float t_start, float p) {
-	float v0 = travel_item->v;
-	float d_dest = p - travel_item->p;
+class Dimension {
+	public:
+	Travel_Item travel_item;
+	Travel travel;
+	Stepper stepper;
 
-	float dd_0 = 0;
-	float dd_accel = 0;
-	float dd_cruise = 0;
-	float dtotal = 0;
-	float dt_0 = 0;
-	float dt_reverse = 0;
-	float dt_accel = 0;
-	float dt_cruise = 0;
-	float dt_decel = 0;
-	float ttop = 0;
-	float dtop = 0;
-
-	if (v0 * d_dest > 0 && (abs(d_dest) < abs(0.5 * maxa * pow((v0 / maxa), 2)))) {
-		// Impossible instruction to decel in time, queue up this instruction next
-		travel->p_next = p;
-		travel->retrigger = true;
-		return;
-	} else {
-		travel->retrigger = false;
+	void set_stepper_pins(int step_pin, int dir_pin) {
+		stepper.step_pin = step_pin;
+		stepper.dir_pin = dir_pin;
 	}
 
-	static int dir;
+	void solve_for_min_time(float maxv, float maxa, float t_start, float p) {
+		float v0 = travel_item.v;
+		float d_dest = p - travel_item.p;
 
-	travel->enable = true;
-	travel->d_dest = d_dest;
-	travel->p_start = travel_item->p;
-	travel->t_start = t_start;
-	travel->v0 = v0;
-	travel->maxa = maxa;
+		float dd_0 = 0;
+		float dd_accel = 0;
+		float dd_cruise = 0;
+		float dtotal = 0;
+		float dt_0 = 0;
+		float dt_reverse = 0;
+		float dt_accel = 0;
+		float dt_cruise = 0;
+		float dt_decel = 0;
+		float ttop = 0;
+		float dtop = 0;
 
-	ttop = maxv / maxa;
-	dtop = 0.5f * maxa * pow(ttop,2);
-
-	if (d_dest > 0) dir = 1;
-	else dir = -1;
-
-	// calculate decel from v0 to 0
-	dt_0 = v0 / maxa;
-	dd_0 = 0.5f * maxa * pow(dt_0, 2);
-	dtotal = abs(d_dest) + abs(dd_0);
-
-	if (v0 * d_dest < 0) {
-		// if going the wrong direction, add decel time to turn around
-		dt_reverse = 2.0 * abs(dt_0);	
-	} else {
-		dt_reverse = 0;
-	}
-
-	if (dtop * 2 > dtotal) {
-		// cruise speed not achieved, use midpoint to start decel
-		dt_decel = sqrt(dtotal / maxa);
-		maxv = maxa * dt_decel;
-		dt_accel = (maxv - abs(v0)) / maxa;
-		dd_accel = abs(v0) * dt_accel + 0.5f * maxa * pow(dt_accel, 2);
-		dt_cruise = 0;
-		dd_cruise = 0;
-	} else {
-		// cruise speed possible, create all 3 phases
-		dt_accel = (maxv - abs(v0)) / maxa;
-		dd_accel = abs(v0) * dt_accel + 0.5f * maxa * pow(dt_accel, 2);
-		dt_decel = ttop;
-		dd_cruise = abs(d_dest) - (dtop + dd_accel);
-		dt_cruise = dd_cruise / maxv;
-	}
-
-	travel->t_accel = dt_reverse + dt_accel;
-	travel->t_cruise = travel->t_accel + dt_cruise;
-	travel->t_decel = travel->t_cruise + dt_decel;
-	travel->d_accel = dd_accel;
-	travel->d_cruise = travel->d_accel + dd_cruise;
-	travel->maxv = maxv;
-	travel->maxa = maxa;
-	travel->dir = dir;
-}
-
-void travel_tick(Travel *travel, TravelItem *travel_item,  float t) {
-	// do not allocate any memory
-	static float dt;
-
-	t = t - travel->t_start;
-
-	if (!travel->enable){
-		// disabled
-		if (travel->retrigger) {
-			travel->retrigger = false;
-			travel_create(travel, travel_item, travel->maxv, travel->maxa, t, travel->p_next);
+		if (v0 * d_dest > 0 && (abs(d_dest) < abs(0.5 * maxa * pow((v0 / maxa), 2)))) {
+			// Impossible instruction to decel in time, queue up this instruction next
+			travel.p_next = p;
+			travel.retrigger = true;
+			return;
+		} else {
+			travel.retrigger = false;
 		}
-	} else if (t > travel->t_decel) {
-		// destination reached, disable
-		travel->enable = false;
-		travel_item->p = travel->p_start + travel->d_dest;
-		travel_item->v = 0;
-	} else if (t < travel->t_accel) {
-		// accelerating
-		travel_item->p = travel->p_start + travel->v0 * t + 0.5f * travel->maxa * travel->dir * pow(t, 2);
-		travel_item->v = travel->v0 + travel->maxa * travel->dir * t;
-	} else if (t < travel->t_cruise) {
-		// cruising
-		dt = t - travel->t_accel;
-		travel_item->p = travel->p_start + travel->d_accel * travel->dir + travel->maxv * travel->dir * (dt);
-		travel_item->v = travel->maxv * travel->dir;
-	} else {
-		// decelerating
-		dt = t - travel->t_cruise;
-		travel_item->p = travel->p_start + travel->d_cruise * travel->dir + travel->maxv * travel->dir * (dt) + 0.5f * (travel->maxa * (-travel->dir)) * pow(dt, 2);
-		travel_item->v = travel->maxv * travel->dir - travel->maxa * travel->dir * dt;
-	}
-}
 
+		static int dir;
+
+		travel.enable = true;
+		travel.d_dest = d_dest;
+		travel.p_start = travel_item.p;
+		travel.t_start = t_start;
+		travel.v0 = v0;
+		travel.maxa = maxa;
+
+		ttop = maxv / maxa;
+		dtop = 0.5f * maxa * pow(ttop,2);
+
+		if (d_dest > 0) dir = 1;
+		else dir = -1;
+
+		// calculate decel from v0 to 0
+		dt_0 = v0 / maxa;
+		dd_0 = 0.5f * maxa * pow(dt_0, 2);
+		dtotal = abs(d_dest) + abs(dd_0);
+
+		if (v0 * d_dest < 0) {
+			// if going the wrong direction, add decel time to turn around
+			dt_reverse = 2.0 * abs(dt_0);	
+		} else {
+			dt_reverse = 0;
+		}
+
+		if (dtop * 2 > dtotal) {
+			// cruise speed not achieved, use midpoint to start decel
+			dt_decel = sqrt(dtotal / maxa);
+			maxv = maxa * dt_decel;
+			dt_accel = (maxv - abs(v0)) / maxa;
+			dd_accel = abs(v0) * dt_accel + 0.5f * maxa * pow(dt_accel, 2);
+			dt_cruise = 0;
+			dd_cruise = 0;
+		} else {
+			// cruise speed possible, create all 3 phases
+			dt_accel = (maxv - abs(v0)) / maxa;
+			dd_accel = abs(v0) * dt_accel + 0.5f * maxa * pow(dt_accel, 2);
+			dt_decel = ttop;
+			dd_cruise = abs(d_dest) - (dtop + dd_accel);
+			dt_cruise = dd_cruise / maxv;
+		}
+
+		travel.t_accel = dt_reverse + dt_accel;
+		travel.t_cruise = travel.t_accel + dt_cruise;
+		travel.t_decel = travel.t_cruise + dt_decel;
+		travel.d_accel = dd_accel;
+		travel.d_cruise = travel.d_accel + dd_cruise;
+		travel.maxv = maxv;
+		travel.maxa = maxa;
+		travel.dir = dir;
+	}
+
+	void tick(float elapsed_u_sec) {
+		static volatile float elapsed_sec;
+		elapsed_sec = (float) elapsed_u_sec / 1000000;
+		solve_tick(elapsed_sec);
+		stepper.p = travel_item.p;
+		stepper_step(&stepper, elapsed_u_sec);	
+	}
+
+	void solve_tick(float t) {
+		// do not allocate any memory
+		static float dt;
+
+		t = t - travel.t_start;
+
+		if (!travel.enable){
+			// disabled
+			if (travel.retrigger) {
+				travel.retrigger = false;
+				solve_for_min_time(travel.maxv, travel.maxa, t, travel.p_next);
+			}
+		} else if (t > travel.t_decel) {
+			// destination reached, disable
+			travel.enable = false;
+			travel_item.p = travel.p_start + travel.d_dest;
+			travel_item.v = 0;
+		} else if (t < travel.t_accel) {
+			// accelerating
+			travel_item.p = travel.p_start + travel.v0 * t + 0.5f * travel.maxa * travel.dir * pow(t, 2);
+			travel_item.v = travel.v0 + travel.maxa * travel.dir * t;
+		} else if (t < travel.t_cruise) {
+			// cruising
+			dt = t - travel.t_accel;
+			travel_item.p = travel.p_start + travel.d_accel * travel.dir + travel.maxv * travel.dir * (dt);
+			travel_item.v = travel.maxv * travel.dir;
+		} else {
+			// decelerating
+			dt = t - travel.t_cruise;
+			travel_item.p = travel.p_start + travel.d_cruise * travel.dir + travel.maxv * travel.dir * (dt) + 0.5f * (travel.maxa * (-travel.dir)) * pow(dt, 2);
+			travel_item.v = travel.maxv * travel.dir - travel.maxa * travel.dir * dt;
+		}
+	}
+
+	bool is_enabled() {
+		return travel.enable;
+	}
+
+	float get_travel_time() {
+		return travel.t_decel;
+	}
+};
